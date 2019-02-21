86a93785a470ca4d238563dfc91506df3ab2eee1
Sudhakar R <sudax3@gmail.com>
2019-02-21 11:54:42 +0530

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace sudax
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string[] args = Environment.GetCommandLineArgs();
+            Clipboard.SetText(args[1]);
+            Application.Current.Shutdown();
+        }
+    }
+}
