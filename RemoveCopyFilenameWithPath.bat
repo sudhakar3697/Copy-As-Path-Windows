26eb70762e02b31734e7be3cf851408d823bff5d
Sudhakar R <sudax3@gmail.com>
2019-09-30 19:29:56 +0530

    Delete RemoveCopyFilenameWithPath.bat

--- RemoveCopyFilenameWithPath.bat
+++
@@ -1,5 +0,0 @@
-set path="%SystemDrive%\Users\Public\Copy filename with path"
-rmdir %path% /s /q
-cd %SystemDrive%\Windows\System32
-REG DELETE "HKCU\Software\Classes\*\shell\Copy filename with path" /f
-PAUSE
